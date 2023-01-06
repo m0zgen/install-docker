a1bf2d71f0c3b2bd83577355d83cd47c6490e6dc
Yevgeniy Goncharov <g.yevgeniy.p@gmail.com>
2023-01-06 20:01:30 +0600

    + echos

--- install.sh
+++ install.sh
@@ -59,17 +59,27 @@ aptUpdate() {
 }
 
 pushDocker() {
+
+    echo "Apt update starting..."
     aptUpdate
+
+    echo "Install packages..."
     sudo apt -y install ca-certificates curl gnupg lsb-release
+
+    echo "Install official Docker keys and Docker repo..."
     sudo mkdir -p /etc/apt/keyrings
     curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
     echo \
         "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/debian \
         $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+    
+    echo "Apt update..."
     aptUpdate
+
+    echo "Install Docker packages..."
     sudo apt -y install docker-ce docker-ce-cli containerd.io docker-compose-plugin
 
-    echo "Done!"
+    echo -e "\nDone!"
     exit 0
 }
 
